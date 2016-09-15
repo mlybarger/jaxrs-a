266cc9e56a382dd2e79e66e8fd490e66fff41d5d
mlybarger <mlybarger@gmail.com>
2016-09-15 16:14:36 -0400

    changes for project.

--- src/main/java/com/example/switchyard/jaxrs_a/Test.java
+++ src/main/java/com/example/switchyard/jaxrs_a/Test.java
@@ -1,7 +1,5 @@
 package com.example.switchyard.jaxrs_a;
 
-import javax.ws.rs.WebApplicationException;
-
 public interface Test {
 	
 	public Foo execute( String parameter );
--- src/main/java/com/example/switchyard/jaxrs_a/TestBean.java
+++ src/main/java/com/example/switchyard/jaxrs_a/TestBean.java
@@ -1,10 +1,14 @@
 package com.example.switchyard.jaxrs_a;
 
+import java.util.Random;
+
+import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
+//import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.log4j.Logger;
+import org.switchyard.component.bean.Reference;
 import org.switchyard.component.bean.Service;
 
 @Service(Test.class)
@@ -12,11 +16,19 @@ public class TestBean implements Test {
 
 	Logger LOG = Logger.getLogger(TestBean.class);
 	
+	Random random = new Random();
+	
+	@Inject
+	@Reference
+	Route route;
+	
 	@Override
 	public Foo execute(String parameter) {
-		LOG.info("execute()");
-		if (true)
+		LOG.info("execute() - start");
+		if (random.nextInt(2) == 1)
 			throw new WebApplicationException( Response.status(Response.Status.NOT_FOUND).entity("could not find item").build());
+		String resp = route.process(parameter);
+		LOG.info("execute() - done");
 		return new Foo();
 	}
 
